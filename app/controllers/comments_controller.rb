2fe66581cccfbbbd5e8593dcecf4f1d9981de857
rails -v <gem update rails --no-ri --no-rdoc>
2016-04-14 04:42:25 +0300

    first commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,17 +3,17 @@ class CommentsController < ApplicationController
     	@article = Article.find(params[:article_id])
     	@comment = @article.comments.create(comment_params)
     	redirect_to article_path(@article)
-  	end
+  end
 
-  	def destroy
+  def destroy
     	@article = Article.find(params[:article_id])
     	@comment = @article.comments.find(params[:id])
     	@comment.destroy
     	redirect_to article_path(@article)
-  	end
+  end
  
-  	private
-    	def comment_params
-      		params.require(:comment).permit(:commenter, :body)
-    	end
+  private
+    def comment_params
+      	params.require(:comment).permit(:commenter, :body)
+    end
 end
